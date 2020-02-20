0504ab255a5834f807add46ba11b7ea8148dbc8a
Gigadeath <arabico157@gmail.com>
2020-02-20 10:11:40 -0300

    teste

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const TOKEN = "4_H25PVovyKOlnmdKrTBsEtuRdlCtRZi";
+const btcValue = require('btc-value');
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+ console.log("ready");
+})
+bot.on("message", function(message){
+ if(message.author.equals(bot.user)) return;
+if (message.content == "!btc") {
+  btcValue().then(value => {
+   btcValue.getPercentageChangeLastDay().then(percentage => {
+    var perc = percentage
+    if (perc > 0){
+        message.channel.send("O preço do  btc é " + value + " $ \nSubiu (ultimo dia): " + perc + "% :hugging:");
+       } else if (perc < 0){
+        message.channel.send("O preço do  btc é " + value + " $ \nDesceu (ultimo dia): " + perc + "% :sob:");
+       } else {
+        message.channel.send("O preço do  btc é " + value + " $ \nManteve (ultimo dia): " + perc + "% :rolling_eyes:");
+       }
+});
+  });
+ }
+});
+bot.login(TOKEN);
